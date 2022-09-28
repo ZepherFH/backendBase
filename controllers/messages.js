785a6a2bfe0997539f08bc78e9ff905789acd130
ZepherFH <marc.gmo17@gmail.com>
2022-09-28 08:17:42 -0500

    Update

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,27 +1,27 @@
 const { request, response } = require("express")
 
 const rootMessage = (req=request, res=response) => {
-    res.send({msg:'Mensajes'})
+    res.status(400).send({msg:'Mensajes'})
 }
 
 const hiMessage =(req=request, res=response) => {
-    res.json({msg:'Hola Mundo'})
+    res.status(401).json({msg:'Hola Mundo'})
 }
 
 const byMessage = (req=request, res=response) => {
-    res.json({msg:'Adios Mundo'})
+    res.status(418).json({msg:'Adios Mundo'})
 }
 
 const postMessage = (req=request, res=response) => {
-    res.json({msg:'Mensaje POST'})
+    res.status(416).json({msg:'Mensaje POST'})
 }
 
 const putMessage = (req=request, res=response) => {
-    res.json({msg:'Mensaje PUT'})
+    res.status(414).json({msg:'Mensaje PUT'})
 }
 
 const deleteMessage = (req=request, res=response) => {
-    res.json({msg:'Mensaje DELETE'})
+    res.status(422).json({msg:'Mensaje DELETE'})
 }
 
     module.exports = {rootMessage, hiMessage, byMessage, postMessage, putMessage, deleteMessage}
