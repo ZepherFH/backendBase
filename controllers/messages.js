fb70c3c4e4d1421ebb057e16e3823e41e2dfbc45
ZepherFH <marc.gmo17@gmail.com>
2022-09-29 11:22:14 -0500

    Parameters

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,7 +1,20 @@
 const { request, response } = require("express")
 
 const rootMessage = (req=request, res=response) => {
-    res.status(400).send({msg:'Mensajes'})
+    const {texto1, texto2}=req.query
+    /*console.log(req.query)El query tiene almacenado el mensaje Hola
+        if(!texto1 || !texto2){
+            res.status(400).json({
+                msg:'No se ha enviado los parametros necesarios, este EndPoint ocupa los parametros texto1 y texto2 '
+     })
+   }*/
+    if (!texto1){
+        res.status(400).json({msg:"Falta el parametro 'texto1'"})
+    }
+    if(!texto2){
+        res.status(400).json({msg:"Falta el parametro 'texto2'"})
+    }
+        res.status(200).json({msg: texto1 + ' ' + texto2})
 }
 
 const hiMessage =(req=request, res=response) => {
